a35200f87f7e77c705a72787b3cd1d0394dde84b
maurauca <maura.quintanilla15903@est.uca.edu.ni>
2023-09-10 23:06:51 -0600

    2 insercion

--- 1.cpp
+++ 1.cpp
@@ -4,7 +4,6 @@ using namespace std;
 int main(int argc, char const *argv[])
 {
     cout << "hola, soy maura y m gusta comer sushi" << endl;
-
-
+    cout << "tambien me gusta comer marisco jej" << endl;
     return 0;
 }
